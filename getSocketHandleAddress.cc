c9668838021072b72877f7f113a1d4df23671be5
Carlos Lopez Jr <clshortfuse@gmail.com>
2016-09-24 13:40:36 -0400

    reduce code

--- getSocketHandleAddress.cc
+++ getSocketHandleAddress.cc
@@ -25,8 +25,7 @@ void getAddress(const FunctionCallbackInfo<Value>& args) {
   void *handleWrap = args[0]->ToObject()->GetAlignedPointerFromInternalField(0);  
   uv_handle_t* handle = getTcpHandle(handleWrap);  
   uv_tcp_t* tcpHandle = (uv_tcp_t*)handle;
-  SOCKET s = tcpHandle->socket;  
-  args.GetReturnValue().Set(Number::New(isolate, s));
+  args.GetReturnValue().Set(Number::New(isolate, tcpHandle->socket));
 }
 
 void getType(const FunctionCallbackInfo<Value>& args) {
